be0035eea0ca88618183b924e5208b28ee756b97
kolinabir <knkolin9@gmail.com>
2024-05-30 01:23:05 +0600

    refactor: Improve error handling and update PUT and DELETE endpoints in server.js

--- practice/server.js
+++ practice/server.js
@@ -42,13 +42,25 @@ app.post("/api/v1/restaurants", async (req, res) => {
   });
 });
 
-app.put("/api/v1/restaurants/:id", (req, res) => {
-  console.log(req.params);
-  console.log(req.body);
+app.put("/api/v1/restaurants/:id", async (req, res) => {
+  const result = await db.query(
+    "UPDATE restaurants SET name = $1, location = $2, price_range = $3 WHERE id = $4 RETURNING *",
+    [req.body.name, req.body.location, req.body.price_range, req.params.id]
+  );
+  res.status(200).json({
+    status: "success",
+    data: result.rows[0],
+  });
 });
 
-app.delete("/api/v1/restaurants/:id", (req, res) => {
-  console.log(req.params);
+app.delete("/api/v1/restaurants/:id", async (req, res) => {
+  const result = await db.query("DELETE FROM restaurants WHERE id = $1", [
+    req.params.id,
+  ]);
+  res.status(204).json({
+    status: "success",
+    message: "Deleted successfully",
+  });
 });
 
 app.listen(port, () => {
